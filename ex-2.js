eca36ea1e1369dbb9baed85e0498e6db5c33d7e9
bodin <bodinsonsawan@gmail.com>
2023-11-27 10:41:03 +0700

    edit(ex1-3)bodin's assignment

--- ex-2.js
+++ ex-2.js
@@ -1,10 +1,17 @@
-// Exercise #2 Product
-
-const product = {
+let product = {
   name: "Xiaomi Air Purifier",
   price: 4000,
   ratings: 2.5,
   photo: null,
 };
+
+ratings = 4.5;
+photo =
+  "https://i01.appmifile.com/webfile/globalimg/products/pc/mi-air-purifier-3H/replace_03.jpg";
+product.code = "IT0101010";
+price = 6000;
+delete product.ratings;
 let socialMediaShare = "facebookShare";
-// Start code here
+
+product[socialMediaShare] = 45.5;
+console.log(product);
